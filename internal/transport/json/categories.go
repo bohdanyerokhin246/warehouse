d2955c7edc85f58d560fe25ad79a7e72995d2e8a
Bodan <erohinbogdan246@gmail.com>
2024-06-03 09:55:12 +0300

    Added handlers to server for CRUD categories.

---
+++ internal/transport/json/categories.go
@@ -0,0 +1,21 @@
+package json
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CreateCategory(c *gin.Context) {
+
+}
+
+func GetCategories(c *gin.Context) {
+
+}
+
+func UpdateCategory(c *gin.Context) {
+
+}
+
+func DeleteCategory(c *gin.Context) {
+
+}
